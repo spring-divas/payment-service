55ffbd5bbdaf63e484658a9ea8c78ce0eb360944
ihnryna <151572051+ihnryna@users.noreply.github.com>
2026-09-19 16:01:54 +0300

    chore(#3): add needed mapstruct dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 
 	implementation("org.mapstruct:mapstruct:1.6.3")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+
+	annotationProcessor ("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
