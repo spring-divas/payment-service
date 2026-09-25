86dd47fd454930b3bfd624f494297975ef4251b6
ihnryna <151572051+ihnryna@users.noreply.github.com>
2026-09-25 12:07:04 +0300

    chore(#13): change image pull policy to Always and add logs for tracking payment creation

--- src/main/java/org/spring/divas/payment/feature/payment/PaymentController.java
+++ src/main/java/org/spring/divas/payment/feature/payment/PaymentController.java
@@ -2,6 +2,8 @@ package org.spring.divas.payment.feature.payment;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,11 +14,14 @@ import java.util.List;
 @RequestMapping("/payment")
 public class PaymentController {
 
+    private static final Logger log = LoggerFactory.getLogger(PaymentController.class);
+
     private final PaymentService paymentService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public PaymentResponseDto create(@Valid @RequestBody PaymentRequestDto dto) {
+        log.info("Received payment creation request for orderId={}", dto.getOrderId());
         return paymentService.create(dto);
     }
 
